bae59b642ecb3f33071b426117c92bac0646494a
Giuseppe Joe Ramundo <46812638+GJRam@users.noreply.github.com>
2022-04-14 15:39:13 +0100

    Update Valid_Parenthesis.cpp

--- 100Liked/Valid_Parenthesis.cpp
+++ 100Liked/Valid_Parenthesis.cpp
@@ -1,26 +1,26 @@
 #include <string>
+
 #include <unordered_map>
+
 #include <stack>
 
 bool isValid(std::string s) {
-    std::unordered_map<char,char> mp;
-    std::stack<char> stk;
-    mp.emplace(')', '(');
-    mp.emplace(']', '[');
-    mp.emplace('}', '{');
-    for (auto const & i:s)
-    {
-        if ( i == ')' || i == ']' || i == '}')
-        {
-            if(stk.size() > 0 && stk.top() == mp[i]) stk.pop();
-            else return false;
-        }
-        else { 
-            stk.emplace(i);
-        }
+  std::unordered_map < char, char > mp;
+  std::stack < char > stk;
+  mp.emplace(')', '(');
+  mp.emplace(']', '[');
+  mp.emplace('}', '{');
+  for (auto
+    const & i: s) {
+    if (i == ')' || i == ']' || i == '}') {
+      if (stk.size() > 0 && stk.top() == mp[i]) stk.pop();
+      else return false;
+    } else {
+      stk.emplace(i);
     }
-    return stk.size() == 0;
+  }
+  return stk.size() == 0;
 }
 int main() {
-    isValid("()()()");
+  isValid("()()()");
 }
